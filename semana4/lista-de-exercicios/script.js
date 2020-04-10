32a14175e8e9817b8156a77dc9d81854e13379ab
Alex <alex.trindade@live.com>
2020-04-09 22:54:30 -0300

    Exercício 1

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,9 @@
+/*
+*
+*   Exercícios de leitura de código
+*   
+    1. Ele executa uma função que converte o dólar em real. Ele solicita ao usuário o valor da
+    cotação em dólar, armazena na variável cotação e retorna o valor da cotação multiplicado
+    pelo valor em dólar fornecido pela variável meu dinheiro. O console.log chama a função e
+    a mensagem que aparece se a cotação for R$ 5,11 é: R$ 510,73.
+*/
