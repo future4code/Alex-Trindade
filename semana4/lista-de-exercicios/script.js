bcfdbfbc5d0973885e8bcb63d4a21fffa4df40b0
Alex <alex.trindade@live.com>
2020-04-12 11:45:31 -0300

    Exercício 2

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -6,4 +6,10 @@
     cotação em dólar, armazena na variável cotação e retorna o valor da cotação multiplicado
     pelo valor em dólar fornecido pela variável meu dinheiro. O console.log chama a função e
     a mensagem que aparece se a cotação for R$ 5,11 é: R$ 510,73.
+
+    2. Este exercício executa uma função que lê o tipo de investimento que o usuário deseja e 
+    o valor investido. No console aparecerá o novo montante em ações, que é o valor investido 
+    (150*1.1) que é igual a 165 e também aparecerá o segundoMontante no valor de 200, que 
+    seria investido no tesouro direto mas como essa opção não existe, será exibida a mensagem
+    default: TIPO DE INVESTIMENTO INFORMADO INCORRETO!
 */
