c0b937a4ad62ccb79e45b01db74cf623bed84328
Alex <alex.trindade@live.com>
2020-08-24 18:09:37 -0300

    exercício 2 completo

--- semana23/aula72/exercicio2.ts
+++ semana23/aula72/exercicio2.ts
@@ -1,17 +1,30 @@
-const stringCompression = (string: string): string => {
-    
-  
-    for (let i = 0; i < string.toLowerCase().length; i++) {
-      if (!charObject[i]) {
-        charObject[i] = 1;
-      } else {
-        charObject[i] += 1;
-      }
+const stringCompression = (term: string): string => {
+  let newString: string[] = [];
+  let previousCharacter: string = term[0];
+  let count: number = 0;
+
+  for (const character of term) {
+    if (character !== previousCharacter) {
+      newString.push(previousCharacter + count);
+      previousCharacter = character;
+      count = 0;
     }
-  
-    console.log(charObject);
-  
-    return "";
-  };
-  
-  stringCompression("aabbb");
+    count++;
+  }
+
+  newString.push(previousCharacter + count);
+
+  let result: string = "";
+
+  for (const iterator of newString) {
+    result += iterator;
+  }
+
+  if (result.length > term.length) {
+    return term;
+  } else {
+    return result;
+  }
+};
+
+stringCompression("aabbb");
